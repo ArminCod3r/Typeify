b099062563e7714409995d956f07dad3f71d4acf
ArminCod3r <Armin.MNSR@gmail.com>
2021-07-27 02:45:40 -0700

    Posts [show]: showing a single post

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,15 @@
+@extends('custom_layouts.app')
+
+@section('content')
+	
+	@if( count($post) > 0 )
+
+		<h2> {{ $post->title }} </h2>
+		<hr/>
+
+		<h4> {!! $post->body !!} </h4>
+
+		<hr/>
+	@endif
+
+@endsection
