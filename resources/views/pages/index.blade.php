e1a8e90643ab1b33700578869929bf6638accddb
ArminCod3r <Armin.MNSR@gmail.com>
2021-07-27 04:03:35 -0700

    Pages [Index]: showing the created-posts (dynamic data)

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,35 +4,35 @@
 @section('content')
 
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-3" align="center">
-			<div><img src="{{ url('/img/tut.png') }}"></div>
-	    	<div align="center" style="font-size: 18px;font-weight: bold;margin-top:10px;">
-	    		Tutorial-1
-	    	</div>
-		</div>
+	
+		@if( count($posts) > 0 )
 
-		<div class="col-md-3" align="center">
-			<div><img src="{{ url('/img/blog.png') }}"></div>
-	    	<div align="center" style="font-size: 18px;font-weight: bold;margin-top:10px;">
-	    		Blog-1
-	    	</div>
-		</div>
+			<div class="row">
+			@foreach($posts as $key=>$post)
+				<?php $key+=1;?>
+				<div class="col-md-3" align="center">
+					<div><img src="{{ url('/img/tut.png') }}"></div>
+			    	<div align="center" style="font-size: 18px;font-weight: bold;margin-top:10px;">
+			    		
+			    		@if( strlen( strip_tags(html_entity_decode($post->title)) ) > 8 )
+							{{ mb_substr($post->title, 0, 8) }}...
+						@else
+						 	{{ $post->title }}
+						@endif
 
-		<div class="col-md-3" align="center">
-			<div><img src="{{ url('/img/tut.png') }}"></div>
-	    	<div align="center" style="font-size: 18px;font-weight: bold;margin-top:10px;">
-	    		Tutorial-2
-	    	</div>
-		</div>
+			    	</div>
+				</div>
 
-		<div class="col-md-3" align="center">
-			<div><img src="{{ url('/img/blog.png') }}"></div>
-	    	<div align="center" style="font-size: 18px;font-weight: bold;margin-top:10px;">
-	    		Blog-2
-	    	</div>
-		</div>
-	</div>	
+				@if($key%4 == 0)
+					</div>
+					<hr/>
+					<div class="row">	
+				@endif
+			@endforeach
+
+		@else
+			<p>No posts found!</p>
+		@endif
 </div>
 
 @endsection
