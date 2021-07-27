9f2de7529f637d34e32031d07614f0a9cb8ea5db
ArminCod3r <Armin.MNSR@gmail.com>
2021-07-27 01:39:10 -0700

    Posts [Model]: 'Post' model created

---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table      = 'posts';
+    public    $primaryKey = 'id';
+    protected $fillable   = [ 'id', 'title', 'body', 'created_at','updated_at' ];
+    public    $timestamps = true;
+}
