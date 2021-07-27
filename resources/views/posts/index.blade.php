f5ee21277917fa78ea4a4a923e0a74472edfa6d4
ArminCod3r <Armin.MNSR@gmail.com>
2021-07-27 02:35:34 -0700

    Posts [index]: showing all the posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,52 @@
+@extends('custom_layouts.app')
+
+@section('content')
+	
+	@if( count($posts) > 0 )
+
+		
+		@foreach($posts as $post)
+			<div class="row">
+
+				<div class="col-md-1"></div>
+
+				<div class="col-md-10 posts-area">
+					<h3>
+						<a href="/posts/{{$post->id}}"> {{ $post->title }} </a>
+					</h3>
+
+
+					<div class="row">
+
+						<div class="col-md-9 posts-content">
+							@if( strlen( strip_tags(html_entity_decode($post->body)) ) > 120 )
+								{!! mb_substr($post->body, 0, 110) !!}...
+							@else
+							 	{!! $post->body !!}
+							@endif
+						</div>
+
+						<div class="col-md-1">
+							<Button class="btn btn-primary"> Edit </Button>
+						</div>
+
+						<div class="col-md-1">
+							<Button class="btn btn-danger"> Delete </Button>
+						</div>
+						
+					</div>
+						
+
+				</div>
+
+				<div class="col-md-1"></div>
+				
+			</div>
+			<br/>
+		@endforeach
+
+	@else
+		<p>No posts found!</p>
+	@endif
+
+@endsection
